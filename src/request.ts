43171994ebb44fcdc7a083dda327c7853366357b
sonbinrob1998 <sonbinrobmathieu@gmail.com>
2019-12-18 10:56:51 +0100

    correction

--- src/request.ts
+++ src/request.ts
@@ -2,12 +2,8 @@
 var req = new XMLHttpRequest();
 
 
-req.open("GET","http://localhost:8080/user", false);
-req.onreadystatechange=function(){
-    if (req.readyState==10 && req.status==200){
-        console.log(JSON.parse)
-    }
-};
+req.open("GET","http://localhost:8080/api/metrics/username/password", false);
+
 req.send(null);
 
 console.log(req.responseText);
