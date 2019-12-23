c06248e482dfb070c0ee7b951c5e2927082a639a
sonbinrob1998 <sonbinrobmathieu@gmail.com>
2019-12-23 16:54:55 +0100

    test 2 travis

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -28,58 +28,58 @@ describe('get', function () {
       })
     })
   })
-describe ('#saveUser', function(){
-    it('should save data or uptate data', function (){
-        dbMet.saveUser("{key: Robi, value: sonbinrob@gmail.com, password: ninja}", function(err:Error| null, result?: Metric[]){
-            expect(err).to.be.null
-            expect(result).to.not.be.undefined
-            expect(result).to.not.be.null
-            expect(result).to.be.an('array');
-            expect('Robi').to.be.a('string');
-            // expect(result).to.have.a.property('key');
-            // expect(result).to.have.a.property('value');
-            // expect(result).to.have.a.property('password');
-            expect(result).to.include({key: 'Robi'})
+// describe ('#saveUser', function(){
+//     it('should save data or uptate data', function (){
+//         dbMet.saveUser("{key: Robi, value: sonbinrob@gmail.com, password: ninja}", function(err:Error| null, result?: Metric[]){
+//             expect(err).to.be.null
+//             expect(result).to.not.be.undefined
+//             expect(result).to.not.be.null
+//             expect(result).to.be.an('array');
+//             expect('Robi').to.be.a('string');
+//             // expect(result).to.have.a.property('key');
+//             // expect(result).to.have.a.property('value');
+//             // expect(result).to.have.a.property('password');
+//             expect(result).to.include({key: 'Robi'})
 
 
 
-        })
-    })
-})
+//         })
+//     })
+// })
 
-describe ('#save', function(){
-    it('should save data or uptate data', function (){
-        dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
-            expect(err).to.be.null
-            expect(result).to.not.be.empty
-        })
-    })
-})
+// describe ('#save', function(){
+//     it('should save data or uptate data', function (){
+//         dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
+//             expect(err).to.be.null
+//             expect(result).to.not.be.empty
+//         })
+//     })
+// })
 
-describe ('#see_all', function(){
-    it('should save data or uptate data', function (){
-        dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
-            expect(err).to.be.null
-            expect(result).to.not.be.empty
-        })
-    })
-})
-describe ('#API_authenticate', function(){
-    it('should save data or uptate data', function (){
-        dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
-            expect(err).to.be.null
-            expect(result).to.not.be.empty
-        })
-    })
-})
-describe ('#delete', function(){
-    it('should save data or uptate data', function (){
-        dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
-            expect(err).to.be.null
-            expect(result).to.not.be.empty
-        })
-    })
-})
+// describe ('#see_all', function(){
+//     it('should save data or uptate data', function (){
+//         dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
+//             expect(err).to.be.null
+//             expect(result).to.not.be.empty
+//         })
+//     })
+// })
+// describe ('#API_authenticate', function(){
+//     it('should save data or uptate data', function (){
+//         dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
+//             expect(err).to.be.null
+//             expect(result).to.not.be.empty
+//         })
+//     })
+// })
+// describe ('#delete', function(){
+//     it('should save data or uptate data', function (){
+//         dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
+//             expect(err).to.be.null
+//             expect(result).to.not.be.empty
+//         })
+//     })
+// })
 
 
 
