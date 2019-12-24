15a3c29ad61cc0064527beda6c37f46d1349601a
sonbinrob1998 <sonbinrobmathieu@gmail.com>
2019-12-24 11:18:40 +0100

    test metrics
    pour pierro lelièvre

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -10,12 +10,35 @@ describe('Metrics', function () {
   before(function () {
     LevelDB.clear(dbPath)
     dbMet = new MetricsHandler(dbPath)
+    
   })
 
   after(function () {
     dbMet.db.close()
   })
 
+//   describe ('saveUser', function() {
+//     it('should save data or uptate data', function (done) {
+//         dbMet.saveUser("user0,robi@gmail.com,password", function(error: Error | null, result?: any)
+//         {
+            
+//             // expect(result).to.not.be.undefined
+//             // expect(result).to.not.be.null
+//             // expect('Robi').to.be.a('string');
+//             // expect('metric:User0:1572876000000').to.be.a('string');
+//             // expect('ninja').to.be.a('string');
+//             //expect(result).to.be.an('array')
+
+//             // expect(result).to.have.a.property('key');
+//             // expect(result).to.have.a.property('value');
+//             // expect(result).to.have.a.property('password');
+//             // expect(result).to.include({key: 'Robi'})
+
+
+
+//         })
+//     })
+// })
   
 
 
@@ -28,26 +51,8 @@ describe('get', function () {
       })
     })
   })
-// describe ('#saveUser', function(){
-//     it('should save data or uptate data', function (){
-//         dbMet.saveUser('{"key":"metric:User0:1572876000000","value":12, password: "ninja"}', function(err:Error| null, result?: Metric[]){
-//             expect(err).to.be.null
-//             expect(result).to.not.be.undefined
-//             expect(result).to.not.be.null
-//             expect('Robi').to.be.a('string');
-//             expect('metric:User0:1572876000000').to.be.a('string');
-//             expect('ninja').to.be.a('string');
-
-//             // expect(result).to.have.a.property('key');
-//             // expect(result).to.have.a.property('value');
-//             // expect(result).to.have.a.property('password');
-//             // expect(result).to.include({key: 'Robi'})
 
 
-
-//         })
-//     })
-// })
 //[{"key":"metric:User0:1572876000000","value":12},{"key":"metric:User0:1572876900000","value":10},{"key":"metric:User0:1572877800000","value":8}]
 // describe ('#save', function(){
 //     it('should save data or uptate data', function (){
@@ -74,17 +79,16 @@ describe('get', function () {
 //         })
 //     })
 // })
-// describe ('#delete', function(){
-//     it('should save data or uptate data', function (){
-//         dbMet.saveUser("Robi", function(err:Error| null, result?: Metric[]){
-//             expect(err).to.be.null
-//             expect(result).to.not.be.empty
-//         })
-//     })
-// })
 
-
-
-
+  
+describe ('#delete', function(){
+    it('should delete data', function (){
+        dbMet.delete( 1, function(err:Error| null, result?: Metric[]){
+            expect(err).to.be.null
+            expect(result).to.be.an('array').that.does.not.include({"key": 1})
+            
+        })
+    })
+})
 
 })
